2f3ee2ffc359586ebd1e1de4ca45ead4223fbbd9
Antoine Kahlouche <antoine@kahlouche.fr>
2025-05-09 23:07:00 +1000

    fetching data from an external source

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,9 +2,18 @@ import { defineCollection, z } from "astro:content";
 import { file } from "astro/loaders";
 
 const products = defineCollection({
-    // https://docs.astro.build/en/guides/content-collections/#building-a-custom-loader
-    // Data can be loaded from anywhere: DB, Api...
-    loader: file("src/assets/products.json"),
+	// https://docs.astro.build/en/guides/content-collections/#building-a-custom-loader
+	// loader: file("src/assets/products.json"),
+	loader: async () => {
+		const response = await fetch(
+			"https://raw.githubusercontent.com/antoinekahlouche/bellroy-elm-astro/refs/heads/main/src/assets/products.json"
+		);
+		const data = await response.json();
+		return data.map((product: any) => ({
+			id: product.id,
+			...product,
+		}));
+	},
 	schema: z.object({
 		id: z.string(),
 		name: z.string(),
